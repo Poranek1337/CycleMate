2bb6515f506baf5cdae7c648d4d046f201a1cb9c
Poranek1337 <kamil.wieczorek112@gmail.com>
2024-06-17 23:33:47 +0200

    Initial Commit

---
+++ CycleMate/CycleMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleMateApp.swift
+//  CycleMate
+//
+//  Created by Poranek on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CycleMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleMateTests/CycleMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CycleMateTests.swift
+//  CycleMateTests
+//
+//  Created by Poranek on 17/06/2024.
+//
+
+import Testing
+@testable import CycleMate
+
+struct CycleMateTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
